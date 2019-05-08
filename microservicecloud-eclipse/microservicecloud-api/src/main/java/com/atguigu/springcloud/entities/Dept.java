b64dcb446bb304f43e0bb2a53b0057ec97a1e351
Rickie <Vincent217@foxmail.com>
2019-05-08 17:41:33 +0800

    update

--- microservicecloud-eclipse/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-eclipse/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -16,14 +16,67 @@ import lombok.experimental.Accessors;
 @Accessors(chain=true)
 public class Dept implements Serializable// entity --orm--- db_table
 {
-	private Long 	deptno; // 主键
-	private String 	dname; // 部门名称
-	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+	public Long 	deptno; // 主键
+	public String 	dname; // 部门名称
+	public String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	
+	
+	
+	/**
+	 * 
+	 */
+	public Dept() {
+	}
+
 	public Dept(String dname)
 	{
 		super();
 		this.dname = dname;
+	}
+
+	/**
+	 * @return the deptno
+	 */
+	public Long getDeptno() {
+		return deptno;
+	}
+
+	/**
+	 * @param deptno the deptno to set
+	 */
+	public Dept setDeptno(Long deptno) {
+		this.deptno = deptno;
+		return this;
+	}
+
+	/**
+	 * @return the dname
+	 */
+	public String getDname() {
+		return dname;
+	}
+
+	/**
+	 * @param dname the dname to set
+	 */
+	public Dept setDname(String dname) {
+		this.dname = dname;
+		return this;
+	}
+
+	/**
+	 * @return the db_source
+	 */
+	public String getDb_source() {
+		return db_source;
+	}
+
+	/**
+	 * @param db_source the db_source to set
+	 */
+	public Dept setDb_source(String db_source) {
+		this.db_source = db_source;
+		return this;
 	}
 	
 	
